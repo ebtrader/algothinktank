eef0f80589775abd9b6ae29487aca4a9eab3e7bb
ebtrader <jsiddique@gmail.com>
2020-12-10 18:11:55 -0500

    Add files via upload

---
+++ get_position_DF.py
@@ -0,0 +1,51 @@
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
+from ibapi.order import *
+from threading import Timer
+import pandas as pd
+
+class TestApp(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self, self)
+        self.df = pd.DataFrame(columns=['symbol', 'sectype', 'position'])
+
+    def error(self, reqId, errorCode, errorString):
+        print("Error: ", reqId, " ", errorCode, " ", errorString)
+
+    def nextValidId(self, orderId):
+        self.start()
+
+    def updatePortfolio(self, contract: Contract, position: float,
+                        marketPrice: float, marketValue: float,
+                        averageCost: float, unrealizedPNL: float,
+                        realizedPNL: float, accountName: str):
+        print("UpdatePortfolio.", "Symbol:", contract.symbol, "SecType:", contract.secType, "Exchange:",
+              contract.exchange,
+              "Position", position, "MarketPrice:", marketPrice, "MarketValue:", marketValue, "AverageCost:",
+              averageCost,
+              "UnrealizedPNL:", unrealizedPNL, "RealizedPNL:", realizedPNL, "AccountName:", accountName)
+
+        self.df.loc[len(self.df)] = [contract.symbol, contract.secType, position]
+        self.df.to_csv('position.csv')
+
+    def start(self):
+        self.reqAccountUpdates(True, "")
+
+    def stop(self):
+        self.reqAccountUpdates(False, "")
+        self.done = True
+        self.disconnect()
+
+
+#execute the classes
+def main():
+
+    app2 = TestApp()
+    app2.connect("127.0.0.1", 4002, 9)  # IB Gateway PaperTrading
+    Timer(3, app2.stop).start()
+    app2.run()
+
+
+if __name__ == "__main__":
+    main()
